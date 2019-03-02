6c53d48796d572dbe0ece6bb3e0392c52f3c7842
Chris Yang <ysm0622@gmail.com>
2019-03-03 01:51:09 +0900

    Update

--- chris/22 - Follow Along Link Highlighter/script.js
+++ chris/22 - Follow Along Link Highlighter/script.js
@@ -2,7 +2,7 @@ const triggers = document.querySelectorAll("a");
 const highlight = document.createElement("span");
 highlight.classList.add("highlight");
 document.body.appnd(highlight);
-function highlightLink() {
-}
+
+function highlightLink() {}
 triggers.forEach(a => a.addEventListener("mouseenter", highlightLink));
 //# sourceMappingURL=script.js.map
